49992576aad3fd21904442231deb400148df1d40
ronitvarnwal <153513638+ronitvarnwal@users.noreply.github.com>
2025-04-17 10:54:45 +0000

    Ggg

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SmartStudyHub/', // <-- Add this line
   plugins: [react()],
   server: {
     host: '0.0.0.0',
   }
-})
+});
