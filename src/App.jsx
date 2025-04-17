f33efb0206acd0cd931907c8b2aa1467a933c602
ronitvarnwal <153513638+ronitvarnwal@users.noreply.github.com>
2025-04-17 10:33:23 +0000

    Hii

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   const [count, setCount] = useState(0)
   return (
     <main>
-        <button onClick=[()=>{setCount{count + 1}}]>clicked {count} times</button>
+        <button onClick={()=>{setCount(count + 1)}}>clicked {count} yaaaa times</button>
     </main>
   )
 }
